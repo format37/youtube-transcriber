3c3250604b1928dc11d8fbb41c62e5c62d1e561e
Alex <format37@gmail.com>
2023-12-28 13:37:19 +0400

    modified:   compose.sh 	modified:   logs.sh

--- compose.sh
+++ compose.sh
@@ -1,4 +1,1 @@
-# sudo docker-compose up --build --force-recreate -d --remove-orphans
-# sudo docker-compose up --build -d
-# echo "Compose approach is deprecated. Please, use build."
-sudo docker-compose up --build --force-recreate --remove-orphans -d
+sudo docker-compose up --build --force-recreate --remove-orphans
--- logs.sh
+++ logs.sh
@@ -1,7 +1,8 @@
 # get id of running container with image name
-container_id=$(sudo docker ps | grep youtube-transcriber_app_1 | awk '{print $1}')
-echo "container id: $container_id"
+# container_id=$(sudo docker ps | grep youtube-transcriber_app_1 | awk '{print $1}')
+# echo "container id: $container_id"
 # get logs
-sudo docker logs -f $container_id
+# sudo docker logs -f $container_id
 # connect to container
 # sudo docker exec -it $container_id /bin/bash
+sudo docker logs -f -t bot_youtube_tts
